f7083c53b09d015c3c77b71ac8f949d64259a874
Simone <simonedescode@gmail.com>
2024-03-02 21:28:06 +0000

    "been coding all day pause"

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,28 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import Button from '../components/Button';
-import { Link } from 'expo-router';
+import { View, Text } from "react-native";
+import React from "react";
+import Button from "../components/Button";
+import { Link } from "expo-router";
 
 const index = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-      <Link href={'/(user)'} asChild>
+    <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+
+      <Link href={"/sign-in"} asChild>
+        <Button text="Sign in" />
+      </Link>
+
+      <Link href={"/sign-up"} asChild>
+        <Button text="Sign up" />
+      </Link>
+
+      <Link href={"/(user)"} asChild>
         <Button text="User" />
       </Link>
-      <Link href={'/(admin)'} asChild>
+
+      <Link href={"/(admin)"} asChild>
         <Button text="Admin" />
       </Link>
+      
     </View>
   );
 };
