29b41932015a90a0b12522b5795182fdf1b24836
Simone <simonedescode@gmail.com>
2024-03-02 12:48:10 +0000

    admin side

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Image, Text, View, Pressable } from "react-native";
 import Colors from "@/src/constants/Colors";
 import { Product } from "../types";
-import { Link } from "expo-router";
+import { Link, useSegments } from "expo-router";
 
 export const defaultPizzaImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
@@ -11,8 +11,12 @@ type ProductListItemProps = {
 };
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
+  const segments = useSegments();
+
+
   return (
-    <Link href={`/menu/${product.id}`} asChild>
+    //this should have been a ./ but its not working so this is a way to work around the problem
+    <Link href={`/${segments[0]}/menu/${product.id}`} asChild> 
       <Pressable style={styles.container}>
         <Image
           source={{ uri: product.image || defaultPizzaImage }}
