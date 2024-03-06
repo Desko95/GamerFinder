9f61f4e5c35c709606f5ecabfe068006627e0f51
Simone <simonedescode@gmail.com>
2024-03-06 15:02:32 +0000

    profile + edit profile

--- src/app/(admin)/profile/_layout.tsx
+++ src/app/(admin)/profile/_layout.tsx
@@ -1,10 +1,33 @@
+import Colors from "@/src/constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
 import { Link, Stack } from "expo-router";
+import { Pressable } from "react-native";
 
-export default function MenuStack() {
+export default function ProfileStack() {
   return (
     <Stack>
-      {/* <Stack.Screen name="index" options={{ title: "Orders" }} /> */}
-      <Stack.Screen name="list" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerRight: () => (
+            <Link href="/(admin)/profile/editProfile" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="gear"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+
+      
     </Stack>
   );
 }
